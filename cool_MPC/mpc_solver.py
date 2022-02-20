34f9030c78e180379df685c7897dca892070f0e1
Gerben Beintema <g.i.beintema@tue.nl>
2022-02-20 20:49:18 +0100

    added multi input support

--- cool_MPC/mpc_solver.py
+++ cool_MPC/mpc_solver.py
@@ -29,23 +29,23 @@ class MPC_solver(object):
         y = self.h_batched(x[None],u[None]).detach().numpy()[0]
         return y
     def dfdx(self, x, u):
-        x = torch.tensor(x,dtype=torch.float32,requires_grad=True)
+        x = torch.tensor(x,dtype=torch.float32)
         u = torch.tensor(u,dtype=torch.float32)
         fnow = lambda x: self.f_batched(x[None],u[None])[0]
         return jacobian(fnow, x).numpy()
     def dfdu(self, x, u):
         x = torch.tensor(x,dtype=torch.float32)
-        u = torch.tensor(u,dtype=torch.float32,requires_grad=True)
+        u = torch.tensor(u,dtype=torch.float32)
         fnow = lambda u: self.f_batched(x[None],u[None])[0]
         return jacobian(fnow, u).numpy()
     def dhdx(self, x, u):
-        x = torch.tensor(x,dtype=torch.float32,requires_grad=True)
+        x = torch.tensor(x,dtype=torch.float32)
         u = torch.tensor(u,dtype=torch.float32)
         hnow = lambda x: self.h_batched(x[None],u[None])[0]
         return jacobian(hnow, x).numpy()
     def dhdu(self, x, u):
         x = torch.tensor(x,dtype=torch.float32)
-        u = torch.tensor(u,dtype=torch.float32,requires_grad=True)
+        u = torch.tensor(u,dtype=torch.float32)
         hnow = lambda u: self.h_batched(x[None],u[None])[0]
         return jacobian(hnow, u).numpy()
 
@@ -75,6 +75,9 @@ class MPC_solver(object):
         return MSE
     
     def solve_it(self, x_init, T, u_inits, y_targets, y_weight=1.0, u_weight=1e-5, u_bounds=None, verbose=0):
+        assert len(u_inits)==T
+        assert len(y_targets)==T
+
         f, h, dfdx, dfdu, dhdx, dhdu = self.f, self.h, self.dfdx, self.dfdu, self.dhdx, self.dhdu
         x0t = np.copy(x_init)
         dxt = np.zeros_like(x0t)
@@ -84,11 +87,20 @@ class MPC_solver(object):
         self.timer.tic('lpv prop')
         errors = []
         for t,(u0t,y_target) in enumerate(zip(u_inits, y_targets)):
-            yt = h(x0t,u0t) + dhdx(x0t,u0t)@dxt + np.dot(dhdu(x0t,u0t),du[t])
+
+            #checks
+            dut = [du[t,i] for i in range(self.nu)] if self.nu is not None else du[t]
+            if self.nu is None:
+                assert isinstance(u0t,(float,int))
+            else:
+                assert len(u0t)==self.nu
+
+            yt = h(x0t,u0t) + dhdx(x0t,u0t)@dxt + np.dot(dhdu(x0t,u0t),dut)
             errors.append(yt - y_target)
             
-            dxt = dfdx(x0t,u0t)@dxt + dfdu(x0t,u0t)*du[t]
+            dxt = dfdx(x0t,u0t)@dxt + np.dot(dfdu(x0t,u0t),dut)
             x0t = f(x0t,u0t)
+        
         self.timer.toc('lpv prop')
 
         self.timer.tic('eqs_make')
@@ -97,6 +109,7 @@ class MPC_solver(object):
             if self.ny is not None:
                 if isinstance(y_weight,(int,float)):
                     y_weight = [y_weight]*self.ny
+                
                 for eqi, y_weight_i in zip(eq,y_weight):
                     sys.add_objective(eqi*y_weight_i)
             else:
@@ -104,22 +117,46 @@ class MPC_solver(object):
 
         if u_weight is not None:
             for t in range(T):
-                sys.add_objective((u_inits[t]+du[t])*u_weight)
+                if self.nu is None:
+                    sys.add_objective((u_inits[t]+du[t])*u_weight)
+                else:
+                    if isinstance(u_weight, (int,float)):
+                        u_weight = [u_weight]*self.nu
+                    for i,u_weight_i in enumerate(u_weight):
+                        sys.add_objective((u_inits[t][i]+du[t,i])*u_weight_i)
         
         if u_bounds is not None:
             for t in range(T):
-                umin, umax = u_bounds
-                sys.add_inequality(u_inits[t]+du[t] <= umax)
-                sys.add_inequality(umin <= u_inits[t]+du[t])
+                if self.nu is None:
+                    umin, umax = u_bounds
+                    if umax is not None: sys.add_inequality(u_inits[t]+du[t] <= umax)
+                    if umin is not None: sys.add_inequality(umin <= u_inits[t]+du[t])
+                else:
+                    assert len(u_bounds)==self.nu
+                    for i,u_bound in enumerate(u_bounds):
+                        umin, umax = u_bound
+                        if umax is not None: sys.add_inequality(u_inits[t][i]+du[t,i] <= umax)
+                        if umin is not None: sys.add_inequality(umin <= u_inits[t][i]+du[t,i])
+        
         self.timer.toc('eqs_make')
         self.timer.tic('solve')
         sys.solve()
         self.timer.toc('solve')
 
-        dusol = [sys[du[i]] for i in range(T)]
+        dusol = [sys[du[t]] for t in range(T)] if self.nu is None else [[sys[du[t,i]] for i in range(self.nu)] for t in range(T)] 
+        dusol = np.array(dusol)
+
         def unow(step_size):
-            u = [step_size*dui + u0i for dui, u0i in zip(dusol, u_inits)]
-            return u if u_bounds is None else np.clip(u, umin, umax)
+            u = step_size*dusol + np.array(u_inits)
+            if self.nu is None:
+                return u if u_bounds is None else np.clip(u, u_bounds[0], u_bounds[1])
+            else:
+                if u_bounds is None:
+                    return u
+                else:
+                    umin, umax = np.array(u_bounds).T #(nu,2)
+                    return np.clip(u, umin, umax)
+
         eval_now = lambda step_size: self.evaluate(x_init, unow(step_size[0]), y_target, y_weight, u_weight)
         out = minimize(fun=eval_now, x0=[self.line_search_start], method='Nelder-Mead', tol=self.step_size_tol)
 
